33df2bd29f865c0fe70bce05b772f5d8a200c922
malinmac1 <malinmac1@gmail.com>
2025-06-28 21:03:17 +0200

    Updated to work with requirements
    Script was updated to work with current x.com structure and manifest was updated to version 3

--- web.js
+++ web.js
@@ -1,35 +1,51 @@
-
-var fa = document.createElement('style');
-    fa.type = 'text/css';
+// Adding font to the document
+let fa = document.createElement('style');
     fa.textContent = '@font-face { font-family: i eat crayons; src: url("'
-        + chrome.extension.getURL('fonts/i eat crayons.ttf')
+        + chrome.runtime.getURL('fonts/i eat crayons.ttf')
         + '"); }';
 document.head.appendChild(fa);
+
 function changeFont(){
-	var content = document.getElementsByClassName('content');
-	for(var i=0; i<content.length;i++){
-		var c = content[i].childNodes[0].nextElementSibling;
-		var username =  c.querySelectorAll('.account-group .username');
-		if(!!username && username[0].innerText == '@realDonaldTrump' ){
-			var tweets = content[i].querySelectorAll('.js-tweet-text-container .TweetTextSize');
-			for(var j =0; j < tweets.length;j++){
-				tweets[j].style.fontFamily = "'i eat crayons', sans-serif";
-				tweets[j].style.color = 'darkred';
-				tweets[j].style.fontSize = '24px';
+	// Selecting all tweets on the page and iterating through them
+	let content = document.querySelectorAll('[data-testid="tweet"]');
+	for(let i=0; i<content.length;i++){
+		let post = content[i]
+
+		// Get all span elements within the tweet
+		let spans = post.querySelectorAll('span');
+		let username = null;
+		
+		// Find the span whose inner text starts with '@'
+		for(let j=0; j<spans.length; j++){
+			if(spans[j].innerText && spans[j].innerText.charAt(0) === '@'){
+				username = spans[j];
+				break;
+			}
+		}
+		
+		// Checking if the username is '@realDonaldTrump'
+		if(username && username.innerText == '@realDonaldTrump'){
+			let tweetTextElements = post.querySelectorAll('[data-testid="tweetText"]');
+
+			// Applying styles to the tweet text
+			if(tweetTextElements.length > 0) {
+				let text = tweetTextElements[0].childNodes[0];
+				text.style.fontFamily = "'i eat crayons', sans-serif";
+				text.style.color = 'darkred';
+				text.style.fontSize = '24px';
 			}
 		}
 	}
 }
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
+
+// Calling changeFont on mutations
+MutationObserver = window.MutationObserver;
+let observer = new MutationObserver(function(mutations, observer) {
     changeFont();
-    // ...
 });
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
+
+// Defining mutation observer to watch for changes in the document
 observer.observe(document, {
   subtree: true,
   attributes: true
-  //...
 });
